bac433c84a8261ba1e6e3c87cbcd20525c9d992e
francois <francois@beaussier.net>
2020-07-26 12:28:19 +1000

    Logistic regression

--- 03-scikit-learn/04.logistic-regression.py
+++ 03-scikit-learn/04.logistic-regression.py
@@ -2,38 +2,38 @@ import numpy as np
 
 plot_learning = True
 
-class Adaline(object):
+# Logistic Regression is not a regression but a classification algorithm
 
-    def __init__(self, eta=0.01, n_iter=50, random_state=1):
+class LogisticRegression(object):
+
+    def __init__(self, eta=0.05, n_iter=100, random_state=1):
         self.eta = eta
         self.n_iter = n_iter
         self.random_state = random_state
-        self.cost = []
 
     def fit(self, X, y):
         rgen = np.random.RandomState(self.random_state)
         self.w = rgen.normal(loc=0.0, scale=0.01, size = 1 + X.shape[1])
-        self.errors = []
+        self.cost = []
 
         for _ in range(self.n_iter):
-            errors = 0
             net_input = self.net_input(X)
             output = self.activation(net_input)
             errors = (y - output)
             self.w[1:] += self.eta * X.T.dot(errors)
             self.w[0] += self.eta * errors.sum()
-            cost = (errors**2).sum() / 2.0
+            cost = -y.dot(np.log(output)) - (1 - y).dot(np.log(1 - output))
             self.cost.append(cost)
         return self
 
     def net_input(self,  X):
         return np.dot(X, self.w[1:]) + self.w[0]
 
-    def activation(self, X):
-        return X
+    def activation(self, z):
+        return 1.0 / (1.0 + np.exp(-np.clip(z, -250, 250)))
 
     def predict(self, X):
-        return np.where(self.activation(self.net_input(X)) >= 0.0, 1 , -1)
+        return np.where(self.activation(self.net_input(X)) >= 0.5, 1.0, 0.0)
 
 # Loading the iris dataset
 import pandas as pd
@@ -45,43 +45,20 @@ df = pd.read_csv(s, header=None, encoding='utf-8')
 print(df.head())
 
 y = df.iloc[0:100, 4].values
-y = np.where(y == 'Iris-setosa', -1, 1)
-
+y = np.where(y == 'Iris-setosa', 0, 1)
 X = df.iloc[0:100, [0, 2]].values
 
-# Training adaline
-
-import matplotlib.pyplot as plt
-
-if plot_learning:
-    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
-
-    a1 = Adaline(eta=0.01, n_iter=10).fit(X, y)
-
-    ax[0].plot(range(1, len(a1.cost) + 1), np.log10(a1.cost), marker='o')
-    ax[0].set_xlabel('Epochs')
-    ax[0].set_ylabel('log(Sum-squared-error)')
-    ax[0].set_title('Adaline - Learning rate 0.01')
-
-    a2 = Adaline(eta=0.0001, n_iter=10).fit(X, y)
-    
-    ax[1].plot(range(1, len(a2.cost) + 1), a2.cost, marker='o')
-    ax[1].set_xlabel('Epochs')
-    ax[1].set_ylabel('Sum-squared-error')
-    ax[1].set_title('Adaline - Learning rate 0.0001')
-
-    plt.show()
-
-# Let's apply feature scalling to help the learning process
+# Feature scalling
 
 X_std = np.copy(X)
 X_std[:,0] = (X[:,0] - X[:,0].mean()) / X[:,0].std()
 X_std[:,1] = (X[:,1] - X[:,1].mean()) / X[:,1].std()
 
-a3 = Adaline(n_iter=15, eta=0.01).fit(X_std, y)
+lr = LogisticRegression(n_iter=100, eta=0.01).fit(X_std, y)
 
 # Visualize the decision boundaries
 
+import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 
 def plot_decision_regions(X, y, classifier, resolution=0.02):
@@ -101,18 +78,19 @@ def plot_decision_regions(X, y, classifier, resolution=0.02):
     for idx, cl in enumerate(np.unique(y)):
         plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1], alpha=0.8, c=colors[idx], marker=markers[idx], label=cl, edgecolor='black')
 
-# Beautiful learning curve!
+# Plot the learning curve and decision regions
+
 fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
 
-ax[0].plot(range(1, len(a3.cost) + 1), np.log10(a3.cost), marker='o')
+ax[0].plot(range(1, len(lr.cost) + 1), lr.cost)
 ax[0].set_xlabel('Epochs')
-ax[0].set_ylabel('log(Sum-squared-error)')
-ax[0].set_title('Adaline - Feature scaling - Learning rate 0.01')
+ax[0].set_ylabel('log-likelihood')
+ax[0].set_title('LogisticRegression - Feature scaling - Learning rate 0.01')
 
-plot_decision_regions(X_std, y, classifier=a3)
-plt.title('Adaline with Feature Scaling')
+plot_decision_regions(X_std, y, classifier=lr)
+plt.title('LogisticRegression with Feature Scaling')
 plt.xlabel('Epochs')
-plt.ylabel('Sum-squared-error')
+plt.ylabel('log-likelihood')
 plt.legend(loc='upper left')
 plt.tight_layout()
 plt.show()
