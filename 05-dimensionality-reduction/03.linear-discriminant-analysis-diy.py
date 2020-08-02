890f64471f8fd1e9b2dc4e707ea7e4de4834b92e
francois <francois@beaussier.net>
2020-08-02 17:16:56 +1000

    Linear discriminant analysis (DIY)

--- 05-dimensionality-reduction/03.linear-discriminant-analysis-diy.py
+++ 05-dimensionality-reduction/03.linear-discriminant-analysis-diy.py
@@ -28,7 +28,8 @@ for label in range(0, 3):
     mean_vecs.append(np.mean(X_train_std[y_train==label], axis=0))
     print(f'MV {label}: {mean_vecs[label]}')
 
-# Within-class scatter matrix Sw
+# Within-class scatter matrix Sw (how far from the mean)
+# Since the data is normalized, it's just thesum of the covariance matrix for each class 
 
 d = 13
 
@@ -37,3 +38,65 @@ for label, mv in zip(range(0, 3), mean_vecs):
     class_scatter = np.cov(X_train_std[y_train==label].T)
     S_W += class_scatter
 print(f'Within-class scatter matrix: {S_W.shape[0]}x{S_W.shape[1]}')
+
+# Between-class scatter matrix
+
+mean_overall = np.mean(X_train_std, axis=0)
+
+S_B = np.zeros((d, d))
+for i, mean_vec in enumerate(mean_vecs):
+    n = X_train_std[y_train == i, :].shape[0]
+    mean_vec = mean_vec.reshape(d, 1)
+    mean_overall = mean_overall.reshape(d, 1)
+    S_B += n * (mean_vec - mean_overall).dot((mean_vec - mean_overall).T)
+print(f'Between-class scatter matrix: {S_B.shape[0]}x{S_B.shape[0]}')
+
+# Solving inv(S_W) x S_B
+
+eigen_vals, eigen_vecs = np.linalg.eig(np.linalg.inv(S_W).dot(S_B))
+
+eigen_pairs = [(np.abs(eigen_vals[i]), eigen_vecs[:, i]) for i in range(len(eigen_vals))]
+eigen_pairs = sorted(eigen_pairs, key=lambda k: k[0], reverse=True)
+
+print('eigen values in decending order:')
+for eigen_val in eigen_pairs:
+    print(eigen_val[0])
+
+# Plot liner discriminant by decreasing eigenvalues
+
+import matplotlib.pyplot as plt
+
+total = sum(eigen_vals.real)
+print('Total:', total)
+discr = [(i / total) for i in sorted(eigen_vals.real, reverse=True)]
+print('Discr:', discr)
+cum_discr = np.cumsum(discr)
+plt.bar(range(1, 14), discr, alpha=0.5, align='center', label='Individual "discriminability"')
+plt.step(range(1, 14), cum_discr, where='mid', label='Cumulative "discriminability"')
+plt.ylabel('"Discriminability" ratio')
+plt.xlabel('Linera discriminants')
+plt.ylim([-0.1, 1.1])
+plt.legend(loc='best')
+plt.tight_layout()
+plt.show()
+
+# Create transformation matrix
+
+w = np.hstack((eigen_pairs[0][1][:, np.newaxis].real, eigen_pairs[1][1][:, np.newaxis].real))
+print('Matrix W:\n', w)
+
+# Transform the data
+
+X_train_lda = X_train_std.dot(w)
+
+# Plot and notice that the classes are linearly separable!
+
+colors = ['r', 'b', 'g']
+markers = ['s', 'x', 'o']
+for l, c, m in zip(np.unique(y_train), colors, markers):
+    plt.scatter(X_train_lda[y_train==l, 0], X_train_lda[y_train==l, 1], c=c, label=l, marker=m)
+plt.xlabel('LD 1')
+plt.ylabel('LD 2')
+plt.legend(loc='upper right')
+plt.tight_layout()
+plt.show()
